9eccd8ef505decf960cee42a3c9cc9f42549d8f1
Nagarjuna Yelisetty <ynagarjuna2012@gmail.com>
2015-08-27 09:20:45 +0530

    Wipro Problem solved for the friends' interview :P

---
+++ src/InterviewProblems/ArrayMaxAdjMemProd.java
@@ -0,0 +1,18 @@
+package InterviewProblems;
+/*
+ Comapany : Wipro
+ Date : 27-08-2015
+ Round : Technical -Elimination
+ Level : Intermediate
+ Concept : Arrays,Iterations,sorting
+ Question : In an array of length N print highest product of the two adjacent elements*/
+public class ArrayMaxAdjMemProd {
+/*  Draft - 1
+    1.Take the Array input
+    2.Multiply the adjacent numbers,store it in an separate array
+    3.Idea :Bubble sort - Perform only one Iteration no need to sort the entire array */
+
+    public static void main(String[] args) {
+
+    }
+}
