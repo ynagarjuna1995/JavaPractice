7c5232ad1d6c85923fe52dca5fac49c2199ca390
Nagarjuna Yelisetty <ynagarjuna2012@gmail.com>
2015-08-29 07:10:19 +0530

    SinglyLinkedList - Implementation in Java from Scratch following Collection Framework Concepts. 1. Created Inner Classes - Node,Iterator 2. Not implementing the remove() with Iterator

--- src/Algorithms/Sorting/DataStructures/SinglyLinkedList.java
+++ src/Algorithms/Sorting/DataStructures/SinglyLinkedList.java
@@ -1,44 +1,89 @@
 package Algorithms.Sorting.DataStructures;
 /*  Basic operations that can be performed on this type of Linked List are
-  1. Adding at the beginning of the Linked List
-  2. Adding at the end of the Linked List
-  3. Remove/Deleting an element in a Linked List
-  4. Adding an element after the specified element
-  5. Adding an element before the specified element
-  6. Getting an element from the Linked LIst given a specified position.
+  1. Add,Get,Remove elements at the beginning of the Linked List
+  2. Add,Get at the end of the Linked List
+  3. Adding an element after the specified element
+  4. Adding an element before the specified element
+  5. Adding an element at the specfied position.
+  6. Getting an element from the Linked List given a specified position
+  7. Remove/Deleting an element in a Linked List
 
   Along with these basic operations there are some very basic operations which we obviously needed while doing the above operations
   1. Checking whether  Linked List isEmpty
   2. Creating a LinkedList without any elements.
-  3. Check whether Linked List contains an element. */
+  3. Check whether Linked List contains an element.
+  4. Position resolution while adding an element at a given position*/
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class SinglyLinkedList<AnyType> implements Iterable<AnyType> {
 
-//  Declare the Head Node for the Linked List
     private Node<AnyType> head;
-
-//  Node Helper Inner Class
-    private static class Node {
-
-//  Every node consists of two members - dataItem,next(reference to the next Node)
-    private AnyType dataItem;
-    private Node<AnyType> next;
-
-//  Constructor
-    public SinglyLinkedList(Node<AnyType> next, AnyType dataItem) {
-        this.next = next;
-        this.dataItem = dataItem;
-    }
-}
-//  Constructor which creates an empty Linked List
-    public SinglyLinkedList() {
+    /************************************************************
+     *      Constructor which creates an empty SinglyLinkedList.
+     *************************************************************/
+    public SinglyLinkedList () {
         head = null;
     }
-//  Check whether Linked List is empty
-    public boolean isEmpty(){
-        return head == null;
+
+    /***************************************************
+    *       Node Class (Must be Static)
+    ****************************************************/
+    private static class Node<AnyType> {
+        private Node<AnyType> next;
+        private AnyType data;
+
+        /*Constructor for the Node Class */
+        public Node (AnyType data, Node<AnyType> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    /******************************************************************
+     *         SinglyLinkedListIterator class implementing iterator
+     ******************************************************************/
+
+    /*The whole idea of the iterator is to provide an access to a private aggregated data and at the same moment
+      hiding the underlying representation. An iterator is Java is an object, and therefore it's implementation requires
+      creating a class that implements the Iterator interface.*/
+
+    private class SinglyLinkedListIterator implements Iterator<AnyType> {
+
+        private Node<AnyType> nextNode;
+
+        public SinglyLinkedList () {
+            Node<AnyType> nextNode = head;
+        }
+
+        public boolean hasNext() {
+           return nextNode != null;
+        }
+
+        /*Return the data in the LinkedList*/
+        public AnyType next() {
+            /*If there is no element throw exception*/
+            if(!hasNext()) throw new NoSuchElementException();
+            /*Returning the data assigning the identifier "res" which contains the element to return */
+            AnyType res = nextNode.data;
+            nextNode = nextNode.next;
+            return res;
+        }
+        /*Not Implementing remove operation through Iterator*/
+        public AnyType remove() {
+            throw new UnsupportedOperationException();
+        }
+
     }
 
 
-
+    /*Method for creating an instance of Iterator i.e., getting the iterator object */
+    public Iterator<AnyType> iterator () {
+        return new SinglyLinkedListIterator();
+    }
 }
+
+
+
 
