eaa6fd454dadb62b805b9a50da07b0c4cdbc6f04
Nagarjuna Yelisetty <ynagarjuna2012@gmail.com>
2015-08-31 15:12:37 +0530

    Add,get,remove operations for the Last element -TEsted

--- src/Algorithms/Sorting/DataStructures/SingleLinkedList.java
+++ src/Algorithms/Sorting/DataStructures/SingleLinkedList.java
@@ -97,11 +97,62 @@ public class SingleLinkedList<anyType> implements Iterable<anyType>{
     /***********************************************************
      *  Add,Remove,get First Element in Linked List
      * **********************************************************/
-    public void addFirst (anyType data) {
-
+    public synchronized void addFirst (anyType data) {
         head = new Node<anyType>(head,data);
         length++;
     }
+
+    public synchronized anyType getFirst () {
+        if (head ==null) throw new NoSuchElementException();
+
+        return head.data;
+    }
+    public synchronized anyType removeFirst() {
+        if (head == null) throw  new UnsupportedOperationException();
+
+        Node<anyType> temp = head;
+        head = head.next;
+        return temp.data;
+    }
+
+    /*******************************************************************
+     *  Add,Remove,Get Last element in Linked List
+     *
+     * ******************************************************************/
+
+   public synchronized void addLast (anyType data) {
+       if (head == null) addFirst(data);
+
+       Node<anyType> temp = head;
+       while (temp.next != null) temp = temp.next;
+       temp.next = new Node<anyType>(null,data);
+
+   }
+   public synchronized anyType getLast () {
+       if (head == null) throw new NoSuchElementException();
+
+       Node<anyType> temp = head;
+       while (temp.next != null) temp = temp.next;
+       return temp.data;
+   }
+   public synchronized anyType removeLast () {
+       if (head == null) throw new UnsupportedOperationException();
+
+       Node<anyType> prev = null, cur = head;
+
+       while (cur.next != null) {
+           prev = cur;
+           cur = cur.next;
+       }
+
+
+       prev.next = null;
+       return cur.data;
+
+
+   }
+
+
     /****************************************************************
     * With out this method we will get a weird error :P
      * Code would be : file_name@4554617c
@@ -122,12 +173,24 @@ public class SingleLinkedList<anyType> implements Iterable<anyType>{
         SingleLinkedList<String> list = new SingleLinkedList<String>();
         if (list.length == 0) System.out.println("An Empty Linked List is created ");
         list.addFirst("K");
-        System.out.println(list.length);
         list.addFirst("O");
         list.addFirst("O");
         list.addFirst("B");
         if (list.length == 4) System.out.println("Inserted elements succesfully using addFirst() ");
-        System.out.println(list.length);
+        System.out.println("Length of LinkedList is "+ list.length);
+        System.out.println("getFirst() Operation");
+        System.out.println(list.getFirst());
+        System.out.println("Printing all the elements in the list");
+        System.out.println(list);
+        System.out.println("After removeFirst() operation");
+        list.removeFirst();
+        System.out.println("After addLast() operation");
+        list.addLast("S");
+        System.out.println("After getLast() operation");
+        System.out.println(list.getLast());
+        System.out.println("After removeLast() operation");
+        System.out.println(list.removeLast());
+        System.out.println("Printing all the elements in Linked list");
         System.out.println(list);
 
 
