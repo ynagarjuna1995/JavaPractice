661092a3582ff684baa3fbc6df0b589f59e4d4ff
Nagarjuna Yelisetty <ynagarjuna2012@gmail.com>
2015-09-15 23:27:01 +0530

    :seedling:  Writing contents to file and rading from it (Only Text file)

---
+++ src/misc/ReadingAFile.java
@@ -0,0 +1,26 @@
+package misc;
+
+
+/**
+ *  Taking an input and writing the ouput is possible so wrting to file is possible
+ *
+ *  We can do it with File class in I/O Package and PrintWriter Class
+ *
+ *  While dealing with I/o Exceptions are common and there are always saviours by the language creates
+ *  with a specific way of handling exceptions with
+ *  1. Try,Catch,Finally blocks
+ *  2. Just simply throw and throws which creates a exception object and split out to the console which results in
+ *     failure in execution of program.*/
+
+
+/**
+ *  1. Here I am creating a text file in a particular location
+ *  2. Writting something programmatically into text file
+ *  3. Again printing it the contents of file to the console*/
+public class ReadingAFile {
+
+    public static void main(String[] args ) throws Exception {
+
+    }
+}
+
